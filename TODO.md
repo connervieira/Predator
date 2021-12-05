99f85234be1de4b9d0e270e352abfe197583e85b
Conner Vieira <connervieira@tutanota.com>
2021-12-05 14:27:27 -0500

    Added a TUI for managing data after analysis

--- TODO.md
+++ TODO.md
@@ -7,16 +7,18 @@ This document explains features that may be added to Predator in the future.
 
 These are the features actively planned for Predator and are likely to be added within the near future.
 
-- [ ] The ability to view plate data in different formats.
-- [ ] The ability to modify the cropping margins for frames.
-- [ ] The ability to natively export detected license plates to an external file.
-- [ ] The ability to format detected license plates as CSV.
+- [X] The ability to view plate data in different formats.
+- [X] The ability to modify the cropping margins for frames.
+- [X] The ability to natively export detected license plates to an external file.
+- [X] The ability to format detected license plates as CSV.
+- [ ] The ability to process multiple sequential videos at once.
 
 
 # Hypothetical
 
 These are features that require much more in-depth changes, and are purely hypothetical.
 
+- [ ] The ability to correlate license plate detects with GPS data from a GPX file.
 - [ ] De-duplication of license plates that aren't identical, but are very similar and likely to be invalid repeats of the same plate.
 - [ ] Support for real-time license plate reading through a Raspberry Pi or similar device.
 - [ ] Real-time alerts for detected license plates.
--- main.py
+++ main.py
@@ -4,8 +4,42 @@ import sys
 
 
 # ----- Configuration Start -----
-crop_script_path = "/path/to/crop_image/script"
+crop_script_path = "/path/to/crop_image/script" # Path to the cropping script in the Predator directory.
+crop_script_path = "/home/cvieira/Software/ProgrammingLanguages/Python/Predator/crop_image"
+
+# Define frame cropping margins. These defaults are designed for a 1440p dashcam facing directly forward. You are highly encouraged to change these to better fit your camera set up.
+left_margin = "700" # How many pixels will be cropped on the left side of the frame.
+right_margin = "700" # How many pixels will be cropped on the right side of the frame.
+top_margin = "700" # How many pixels will be cropped on the top of the frame.
+bottom_margin = "300" # How many pixels will be cropped on the bottom of the frame.
 # ----- Configuration End -----
+
+
+# Define the function that will be used to clear the screen.
+def clear():
+    os.system("clear")
+
+
+# Define the function that will be used to save files for exported data.
+def save_to_file(file_name, contents):
+    fh = None
+    success = False
+    try:
+        fh = open(file_name, 'w')
+        fh.write(contents)
+        success = True   
+        print("Successfully saved at export.txt in the project directory.")
+    except IOError as e:
+        success = False
+        print(e)
+        print("Failed to save!")
+    finally:
+        try:
+            if fh:
+                fh.close()
+        except:
+            success = False
+    return success
 
 
 
@@ -19,7 +53,7 @@ framerate = float(input("Please enter how many seconds you want to wait between 
 # Split the supplied video into individual frames based on the user's input
 frame_split_command = "mkdir " + root + "/frames; ffmpeg -i " + root + "/" + video + " -r " + str(1/framerate) + " " + root + "/frames/output%04d.png -loglevel quiet"
 
-os.system("clear")
+clear()
 print("Splitting video into discrete images...")
 os.system(frame_split_command)
 print("Done.\n")
@@ -37,7 +71,7 @@ print("Done.\n")
 # Crop the individual frames to make license plate recognition more efficient and accurate.
 print("Cropping individual frames...")
 for frame in frames:
-    os.system(crop_script_path + " " + root + "/frames/" + frame + " 600 600 700 300")
+    os.system(crop_script_path + " " + root + "/frames/" + frame + " " + left_margin + " " + right_margin + " " + top_margin + " " + bottom_margin)
 print("Done.\n")
 
 
@@ -52,6 +86,7 @@ for frame in frames:
 print("Done.\n")
 
 
+raw_lpr_scan = lpr_scan # Save the data collected to a variable before sanitizing and validating it so we can access the raw data later.
 
 
 # Check the possible plate IDs and validate based on general Ohio plate formatting.
@@ -94,5 +129,107 @@ plates_detected = list(dict.fromkeys(plates_detected))
 print("Done.\n")
 
 
-input("Press enter to view results...")
-print(plates_detected)
+
+# Analysis has been completed. Next, the user will choose what to do with the analysis data.
+
+
+input("Press enter to continue...")
+
+while True:
+    clear()
+
+    print("Please select an option")
+    print("0. Quit")
+    print("1. View data")
+    print("2. Export data")
+    print("3. Manage raw analysis data")
+
+    selection = input("Selection: ")
+    clear()
+
+
+    if (selection == "0"):
+        print("Shutting down")
+        break
+
+    elif (selection == "1"):
+        print("Please select an option")
+        print("0. Back")
+        print("1. View raw Python data")
+        print("2. View as list")
+        print("3. View as CSV")
+        
+        selection = input("Selection: ")
+
+        if (selection == "0"):
+            print("Returning to main menu.")
+
+        elif (selection == "1"): # Print raw plate data.
+            print(plates_detected)
+
+        elif (selection == "2"): # Print plate data as a list with one plate per line.
+            for plate in plates_detected:
+                print(plate)
+
+        elif (selection == "3"): # Print plate data as CSV (add a comma after each plate)
+            for plate in plates_detected:
+                print(plate + ",")
+
+        else:
+            print("Unrecognized option.")
+
+        input("\nPress enter to continue...") # Wait for the user to press enter before repeating the menu loop.
+        
+    elif (selection == "2"):
+        print("Please select an option")
+        print("0. Back")
+        print("1. Export raw Python data")
+        print("2. Export as list")
+        print("3. Export as CSV")
+        
+        selection = input("Selection: ")
+
+
+        export_data = "" # Create a blank variable to store the export data.
+
+        if (selection == "0"):
+            print("Returning to main menu.")
+
+        elif (selection == "1"): # Export raw plate data.
+            export_data = str(plates_detected)
+
+            save_to_file(root + "/export.txt", export_data) # Save to disk.
+        
+        elif (selection == "2"): # Export plate data as a list with one plate per line.
+            for plate in plates_detected:
+                export_data = export_data + plate + "\n"
+
+            save_to_file(root + "/export.txt", export_data) # Save to disk.
+
+        elif (selection == "3"): # Export plate data as CSV (add comma after each plate)
+            for plate in plates_detected:
+                export_data = export_data + plate + ",\n"
+
+            save_to_file(root + "/export.txt", export_data) # Save to disk.
+
+        input("\nPress enter to continue...") # Wait for the user to press enter before repeating the menu loop.
+
+    elif (selection == "3"):
+        print("Please select an option")
+        print("0. Back")
+        print("1. View raw data")
+        print("2. Export raw data")
+
+        selection = input("Selection: ")
+
+        if (selection == "0"):
+            print("Returning to main menu.")
+
+        elif (selection == "1"):
+            print(raw_lpr_scan)
+
+        elif (selection == "2"):
+            save_to_file(root + "/export.txt", str(raw_lpr_scan)) # Save to disk.
+            
+
+        input("\nPress enter to continue...") # Wait for the user to press enter before repeating the menu loop.
