aa5252704c8e5d19ac3f6574d4486ccd06b12aa3
Conner Vieira <connervieira@tutanota.com>
2022-02-12 14:07:40 -0500

    Created utils.py to organize main.py

---
+++ utils.py
@@ -0,0 +1,200 @@
+# This script contains several funtions and classes used in main.py
+
+
+# This function will be used to process GPX files into a Python dictionary.
+def process_gpx(gpx_file):
+    gpx_file = open(gpx_file, 'r') # Open the GPX document.
+
+    xmldoc = minidom.parse(gpx_file) # Load the full XML GPX document.
+
+    track = xmldoc.getElementsByTagName('trkpt') # Get all of the location information from the GPX document.
+    timing = xmldoc.getElementsByTagName('time') # Get all of the timing information from the GPX document.
+
+    gpx_data = {} 
+
+    for i in range(0, len(timing)): # Iterate through each point in the GPX file.
+        point_lat = track[i].getAttribute('lat') # Get the latitude for this point.
+        point_lon = track[i].getAttribute('lon') # Get the longitude for this point.
+        point_time = str(timing[i].toxml().replace("<time>", "").replace("</time>", "").replace("Z", "").replace("T", " ")) # Get the time for this point in human readable text format.
+
+        point_time = round(time.mktime(datetime.datetime.strptime(point_time, "%Y-%m-%d %H:%M:%S").timetuple())) # Convert the human readable timestamp into a Unix timestamp.
+
+        gpx_data[point_time] = {"lat":point_lat, "lon":point_lon} # Add this point to the decoded GPX data.
+
+
+    return gpx_data
+
+
+
+
+# Define the function that will be used to clear the screen.
+def clear():
+    os.system("clear")
+
+
+
+# Define the function that will be used to save files for exported data.
+def save_to_file(file_name, contents):
+    fh = None
+    success = False
+    try:
+        fh = open(file_name, 'w')
+        fh.write(contents)
+        success = True   
+        print("Successfully saved at " + file_name + ".")
+    except IOError as e:
+        success = False
+        print(e)
+        print("Failed to save!")
+    finally:
+        try:
+            if fh:
+                fh.close()
+        except:
+            success = False
+    return success
+
+
+
+# Define the fuction that will be used to add to the end of a file.
+def add_to_file(file_name, contents):
+    fh = None
+    success = False
+    try:
+        fh = open(file_name, 'a')
+        fh.write(contents)
+        success = True   
+        print("Successfully saved at " + file_name + ".")
+    except IOError as e:
+        success = False
+        print(e)
+        print("Failed to save!")
+    finally:
+        try:
+            if fh:
+                fh.close()
+        except:
+            success = False
+    return success
+
+
+def validate_plate(plate, template):
+    plate_valid = True # By default, the plate is valid, until we find a character that doesn't align.
+
+    if (len(template) == len(plate)): # Make sure the template and plate are the same length. If so, continue with validation. Otherwise, automatically invalidate the plate, and skip the rest of the validation process.
+        for x in range(len(template)):
+            if (template[x].isalpha() == plate[x].isalpha() or template[x].isnumeric() == plate[x].isnumeric()): # If this character is alphabetical in both the template and plate, or if this character is numeric in both the template and plate, then this character is valid.
+                # This characteris valid, so don't change anything.
+                pass
+            else:
+                # This character doesn't match between the template and plate, so mark the plate as invalid.
+                plate_valid = False
+    else:
+        plate_valid = False
+
+    return plate_valid # Return the results of the plate validation
+
+
+
+# This function is used to download and process plain-text lists of license plates over a network.
+def download_plate_database(url):
+    raw_download_data = urllib.request.urlopen(url).read() # Save the raw data from the URL to a variable.
+
+    # Process the downloaded data step by step to form a list of all of the plates in the database.
+    processed_download_data = str(raw_download_data) # Convert the downloaded data to a string.
+    processed_download_data = processed_download_data.replace("\\n", "\n") # Replace the indicated line-breaks with true line-breaks.
+    processed_download_data = re.sub('([^A-Z0-9\\n\\r\*\-\?\\[\\]])+', '', processed_download_data) # Remove all chracters except capital letters, numbers, and line-breaks.
+
+    download_data_list = processed_download_data.split() # Split the downloaded data line-by-line into a Python list.
+
+    return download_data_list
+
+
+
+# This is a simple function used to display large ASCII shapes.
+def display_shape(shape):
+    if (shape == "square"):
+        print(style.bold)
+        print("######################")
+        print("######################")
+        print("######################")
+        print("######################")
+        print("######################")
+        print("######################")
+        print("######################")
+        print("######################")
+        print("######################")
+        print("######################")
+        print("######################")
+        print("######################")
+        print(style.end)
+
+    elif (shape == "circle"):
+        print(style.bold)
+        print("        ######")
+        print("     ############")
+        print("   ################")
+        print("  ##################")
+        print(" ####################")
+        print("######################")
+        print("######################")
+        print("######################")
+        print(" ####################")
+        print("  ##################")
+        print("   ################")
+        print("     ############")
+        print("        ######")
+        print(style.end)
+
+    elif (shape == "triangle"):
+        print(style.bold)
+        print("           #")
+        print("          ###")
+        print("         #####")
+        print("        #######")
+        print("       #########")
+        print("      ###########")
+        print("     #############")
+        print("    ###############")
+        print("   #################")
+        print("  ###################")
+        print(" #####################")
+        print("#######################")
+        print(style.end)
+
+    elif (shape == "diamond"):
+        print(style.bold)
+        print("           #")
+        print("          ###")
+        print("         #####")
+        print("        #######")
+        print("       #########")
+        print("      ###########")
+        print("      ###########")
+        print("       #########")
+        print("        #######")
+        print("         #####")
+        print("          ###")
+        print("           #")
+        print(style.end)
+
+
+# Define some styling information
+class style:
+    # Define colors
+    purple = '\033[95m'
+    cyan = '\033[96m'
+    blue = '\033[94m'
+    green = '\033[92m'
+    yellow = '\033[93m'
+    gray = '\033[1;37m'
+    red = '\033[91m'
+
+    # Define text decoration
+    bold = '\033[1m'
+    underline = '\033[4m'
+    italic = '\033[3m'
+    faint = '\033[2m'
+
+    # Define styling end marker
+    end = '\033[0m'
