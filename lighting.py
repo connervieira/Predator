37b310ac9f8d96232b8f5c8db403b6dde6237e96
Conner Vieira <connervieira@tutanota.com>
2022-03-04 11:07:13 -0500

    Made progress on status lighting

--- lighting.py
+++ lighting.py
@@ -1,7 +1,7 @@
-import validators
-import urllib.request
+import validators # Required to validating URLs
+import urllib.request # Required to send network requests
 
-import utils
+import utils # Import the utils.py script
 style = utils.style # Load the style from the utils script
 clear = utils.clear # Load the screen clearing function from the utils script
 
@@ -10,13 +10,14 @@ config = json.load(open(predator_root_directory + "/config.json")) # Load the co
 
 
 def update_status_lighting(url_id):
-    if (validators.url(config["realtime"]["status_lighting"][url_id])): # Check to make sure the URL ID supplied actually resolves to a valid URL in the configuration database.
-        try: # Try sending a request to the webook.
-            webhook_response = urllib.request.urlopen(config["realtime"]["status_lighting"][url_id]).getcode() # Save the raw data from the request to a variable.
+    status_lighting_update_url = config["realtime"]["status_lighting_base_url"].replace("[U]", config["realtime"]["status_lighting"]) # Prepare the URL where a request will be sent in order to update the status lighting.
+    if (validators.url(status_lighting_update_url)): # Check to make sure the URL ID supplied actually resolves to a valid URL in the configuration database.
+        try: # Try sending a request to the URL.
+            webhook_response = urllib.request.urlopen(status_lighting_update_url).getcode() # Save the raw data from the request to a variable.
         except Exception as e:
             webhook_response = e
 
-        if (str(webhook_response) != "200"): # If the webhook didn't respond with a 200 code; Warn the user that there was an error.
+        if (str(webhook_response) != "200"): # If the server didn't respond with a 200 code; Warn the user that there was an error.
             print(style.yellow + "Warning: Unable to update status lighting. Response code: " + str(webhook_response.getcode()) + style.end)
     else:
-            print(style.yellow + "Warning: Unable to update status lighting. Invalid URL configured for " + url_id + style.end)
+            print(style.yellow + "Warning: Unable to update status lighting. Invalid URL configured for " + url_id + style.end) # Display a warning that the URL was invalid, and no network request was sent.
