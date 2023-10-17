19497e888621d8a7ff321ff89140a4fa8fcea291
Conner Vieira <cvieira@v0lttech.com>
2023-10-17 10:04:04 -0400

    Added comments to alprstream.py

--- alprstream.py
+++ alprstream.py
@@ -44,21 +44,21 @@ def alpr_stream():
         alpr_command = "alpr -j -n " + str(config["general"]["alpr"]["validation"]["guesses"]) + " " + config["realtime"]["image"]["camera"]["device"] + " >> " + alpr_stream_file_location # Set up the Phantom ALPR command.
     alpr_stream_process = os.popen(alpr_command) # Execute the ALPR command.
 
-def alpr_stream_maintainer():
+def alpr_stream_maintainer(): # This function runs an endless loop that maintains
     global queued_plate_reads
     while True:
-        time.sleep(1)
+        time.sleep(0.3) # Delay for a short period of time before each loop so that the ALPR stream has time to output some results.
         stream_file = open(alpr_stream_file_location) # Open the ALPR stream file.
         stream_file_contents = stream_file.readlines() # Read the stream file line by line.
         stream_file.close() # Close the ALPR stream file.
         save_to_file(alpr_stream_file_location, "", True) # Erase the contents of the ALPR stream file.
 
-        for message in stream_file_contents:
-            message = json.loads(message)
-            for plate in message["results"]:
-                queued_plate_reads.append(plate)
+        for message in stream_file_contents: # Iterate through each line in the loaded stream file contents.
+            message = json.loads(message) # Parse each line into JSON.
+            for plate in message["results"]: # Iterate through each license plate in this line.
+                queued_plate_reads.append(plate) # Add each license plate to the license plate queue.
 
-def start_alpr_stream():
+def start_alpr_stream(): # This function starts the ALPR stream threads.
     debug_message("Opening ALPR message stream")
     alpr_stream_thread = threading.Thread(target=alpr_stream, name="ALPRStream") # Initialize the ALPR stream thread.
     alpr_stream_thread.start() # Start the ALPR stream thread.
@@ -66,8 +66,8 @@ def start_alpr_stream():
     alpr_stream_maintainer_thread.start() # Start the ALPR stream maintainer thread.
 
 
-def alpr_get_queued_plates():
+def alpr_get_queued_plates(): # This function is used to fetch the latest queue of detected license plates.
     global queued_plate_reads
     results_to_return = queued_plate_reads
-    queued_plate_reads = []
+    queued_plate_reads = [] # Clear the license plate queue.
     return results_to_return
